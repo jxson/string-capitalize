c2f4fe8a15e8fe747c2b4b375a50d3ec2b16ce36
Jason Campbell <jason@artifact.sh>
2013-11-04 23:05:45 -0800

    Adds working code and tests

--- test-capitalize.js
+++ test-capitalize.js
@@ -3,5 +3,22 @@ const assert = require('assert')
 const capitalize = require('./index.js')
 
 describe('capitalize(string)', function(){
+  it('capitalizes words', function(){
+    assert.equal(capitalize('word'), 'Word')
+  })
 
+  it('capitalizes phrases', function(){
+    assert.equal(capitalize('word up'), 'Word up')
+  })
+
+  it('capitalizes all caps', function(){
+    assert.equal(capitalize('WORD UP'), 'Word up')
+  })
+
+  it('handles bad input gracefully', function(){
+    assert.equal(capitalize(), '')
+    assert.equal(capitalize(''), '')
+    assert.equal(capitalize(null), '')
+    assert.equal(capitalize(undefined), '')
+  })
 })
