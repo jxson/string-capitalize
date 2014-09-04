d9c8176acbe7c754d08ad10ac63a93fc60056c89
Jason Campbell <jasoncampbell@google.com>
2014-09-03 23:48:12 -0700

    Updates test harness and makefile

--- Makefile
+++ Makefile
@@ -1,9 +1,25 @@
 
-VERSION=patch
+PATH := node_modules/.bin:$(PATH)
+SHELL := /bin/bash -e -o pipefail
+
+VERSION := patch
+
+node_modules: package.json
+	@npm prune
+	@npm install
+	@touch node_modules
+
+clean:
+	@$(RM) -fr test/source/build
+	@$(RM) -fr node_modules $(STANDALONE).js
+	@$(RM) -fr npm-debug.log
+
+test: node_modules
+	prova test-*.js
 
 release:
 	npm version $(VERSION)
 	git push && git push --tags
 	npm publish
 
-.PHONY: release
+.PHONY: clean release test
--- test-capitalize.js
+++ test-capitalize.js
@@ -1,24 +1,14 @@
 
-var assert = require('assert')
-  , capitalize = require('./index.js')
+var test = require('prova')
+var capitalize = require('./')
 
-describe('capitalize(string)', function(){
-  it('capitalizes words', function(){
-    assert.equal(capitalize('word'), 'Word')
-  })
-
-  it('capitalizes phrases', function(){
-    assert.equal(capitalize('word up'), 'Word up')
-  })
-
-  it('capitalizes all caps', function(){
-    assert.equal(capitalize('WORD UP'), 'Word up')
-  })
-
-  it('handles bad input gracefully', function(){
-    assert.equal(capitalize(), '')
-    assert.equal(capitalize(''), '')
-    assert.equal(capitalize(null), '')
-    assert.equal(capitalize(undefined), '')
-  })
+test('capitalize(string)', function(assert){
+  assert.equal(capitalize('word'), 'Word')
+  assert.equal(capitalize('word up'), 'Word up')
+  assert.equal(capitalize('WORD UP'), 'Word up')
+  assert.equal(capitalize(), '')
+  assert.equal(capitalize(''), '')
+  assert.equal(capitalize(null), '')
+  assert.equal(capitalize(undefined), '')
+  assert.end()
 })
